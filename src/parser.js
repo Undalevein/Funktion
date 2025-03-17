55f7a74e6e392e8347ed2f0b6585279f9b05f438
Wesley Ng <73354635+Undalevein@users.noreply.github.com>
2025-03-17 16:09:53 -0700

    Changed funktion ohm

--- src/parser.js
+++ src/parser.js
@@ -1,11 +1,12 @@
-import * as fs from "node:fs"
-import * as ohm from "ohm-js"
+import * as fs from "node:fs";
+import * as ohm from "ohm-js";
 
-const grammar = ohm.grammar(fs.readFileSync("src/funktion.ohm"))
+const grammar = ohm.grammar(fs.readFileSync("src/funktion.ohm"));
 
 // Returns the Ohm match if successful, otherwise throws an error
 export default function parse(sourceCode) {
-  const match = grammar.match(sourceCode)
-  if (!match.succeeded()) throw new Error(match.message)
-  return match
+  const match = grammar.match(sourceCode);
+  console.log(match);
+  if (!match.succeeded()) throw new Error(match.message);
+  return match;
 }
